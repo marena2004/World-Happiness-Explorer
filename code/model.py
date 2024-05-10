1ebbd442c7f73d76eb493a35329cb25c6a9b3f82
suparak <suparak.may@gmail.com>
2024-05-11 00:02:18 +0700

    clean data

--- code/model.py
+++ code/model.py
@@ -4,20 +4,32 @@ import pandas as pd
 class DataLoader:
     """Handles loading, cleaning, and storing of CSV data."""
 
-    def __init__(self, filename):
-        self.filename = filename
+    def __init__(self):
         self.data = None
 
     def load_data(self):
         try:
-            self.data = pd.read_csv(self.filename)
+            self.data = pd.read_csv("WorldHappiness2015-2019.csv")
         except FileNotFoundError:
             raise FileNotFoundError("CSV file not found.")
 
         self.clean_data()
 
     def clean_data(self):
-        pass
+        # Rename index
+        self.data.rename(index={'Taiwan Province of China': 'Taiwan',
+                                'Hong Kong S.A.R., China': 'Hong Kong',
+                                'Trinidad & Tobago': 'Trinidad and Tobago',
+                                'Northern Cyprus': 'North Cyprus',
+                                'North Macedonia': 'Macedonia'}, inplace=True)
+
+        # Add the correct Region value to changed index
+        self.data.loc['Taiwan', 'Region'] = 'Eastern Asia'
+        self.data.loc['Hong Kong', 'Region'] = 'Eastern Asia'
+        self.data.loc['Trinidad and Tobago', 'Region'] = 'Latin America and Caribbean'
+        self.data.loc['North Cyprus', 'Region'] = 'Central and Eastern Europe'
+        self.data.loc['Macedonia', 'Region'] = 'Central and Eastern Europe'
+        self.data.loc['Gambia', 'Region'] = 'Sub-Saharan Africa'
 
     def get_data(self):
         return self.data
